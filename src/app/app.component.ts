638137db6078cc383b969f1ab6f4355a191f9795
lakshana36 <saravananlakshana@gmail.com>
2018-07-21 23:36:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Course Registration';
+  // durations=[];
+  // name:string;
+  // constructor(){
+  //   this.durations = [
+  //     {name: '3months'},
+  //     {name: '6 months'},
+  //     {name: '9 months'},
+  //     {name: '1 year'}
+  //   ];
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{InputTextModule} from 'primeng/inputtext';
 import { AppComponent } from './app.component';
-
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import{CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    MessageModule,
+    MessagesModule,
+    CheckboxModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
